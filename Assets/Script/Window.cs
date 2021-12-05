5ff25fce08fd97207f90b6b174feb75d617a7216
ofgonen <ofgonen@gmail.com>
2021-12-05 20:47:01 +0300

    Window: Vaulting enemies
    - Implement IInteractable method name changes

--- Assets/Script/Window.cs
+++ Assets/Script/Window.cs
@@ -3,26 +3,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.AI.Navigation;
 using FFStudio;
 using NaughtyAttributes;
 
 public class Window : MonoBehaviour, IInteractable
 {
 #region Fields
+    [ BoxGroup( "Setup" ) ] public NavMeshLink navMeshLink;
     [ BoxGroup( "Setup" ) ] public BoxCollider colliderHealth;
     [ BoxGroup( "Setup" ) ] public BoxCollider colliderSeek;
+    [ BoxGroup( "Setup" ) ] public ColliderListener_Stay_EventRaiser colliderListener_Seek_Stay;
 
     // Private Fields \\
     private bool isAlive = true;
+	private float lastVaultTime;
 
-    // Delegates \\
-    private event UnityMessage onDeath;
+	// Delegates \\
+	private event UnityMessage onDeath;
 #endregion
 
 #region Properties
 #endregion
 
 #region Unity API
+    private void OnDisable()
+    {
+		colliderListener_Seek_Stay.ClearEventList();
+	}
 #endregion
 
 #region API
@@ -36,12 +44,12 @@ public class Window : MonoBehaviour, IInteractable
 		return Vector3.zero;
 	}
 
-    public void Deposit( int count )     
+    public void GetDeposit( int count )     
     {
 
     }
 
-    public void Damage( int count )
+    public void GetDamage( int count )
     {
         FFLogger.Log( "Damage: " + count );
     }
@@ -53,37 +61,64 @@ public class Window : MonoBehaviour, IInteractable
 
 	public void Subscribe_OnDeath( UnityMessage onDeathDelegate )
     {
-        //! DO onDeath = null for clearin the invoke list
+        //! DO onDeath = null for clearing the invoke list
 		onDeath += onDeathDelegate;
 	}
-
 #endregion
 
 #region Implementation
+    [ Button() ]
+    private void Die()
+    {
+        colliderHealth.enabled = false;
+        isAlive                = false;
+
+		onDeath();
+		onDeath = null;
+
+		colliderListener_Seek_Stay.triggerEvent += VaultInEnemies;
+	}
+
+    [ Button() ]
+    private void Revive()
+    {
+        // Toggle for re-activating OnTriggerEnter events
+        colliderSeek.enabled = false;
+        colliderSeek.enabled = true;
+
+        // Enable healt collider since it can dake damage
+        colliderHealth.enabled = true;
+		isAlive = true;
+
+		// 
+		colliderListener_Seek_Stay.triggerEvent -= VaultInEnemies;
+	}
+
+    private void VaultInEnemies( Collider other )
+    {
+		var canVault = lastVaultTime + GameSettings.Instance.window_cooldown_vault < Time.time;
+
+		if( !canVault ) return; // Cooldown is not met
+
+		// Vault enemies in
+		var enemy = other.GetComponentInParent< Enemy >();
+
+		//Find vault position
+		var position = new Vector3
+		(
+			Random.Range( navMeshLink.endPoint.x - navMeshLink.width, navMeshLink.endPoint.x + navMeshLink.width ),
+			0,
+			Random.Range( navMeshLink.endPoint.z / 2, navMeshLink.endPoint.z )
+		);
+
+		enemy.Vault( transform.TransformPoint( position ) );
+
+		lastVaultTime = Time.time;
+	}
 #endregion
 
 #region Editor Only
 #if UNITY_EDITOR
-    [ Button() ]
-    private void Test_Death()
-    {
-        colliderHealth.enabled = false;
-
-        isAlive = false;
-
-		onDeath();
-		onDeath = null;
-	}
-
-    [ Button() ]
-    private void Test_Alive()
-    {
-		isAlive = true;
-
-        colliderSeek.enabled = false;
-        colliderHealth.enabled = true;
-        colliderSeek.enabled = true;
-	}
 #endif
 #endregion
 }
