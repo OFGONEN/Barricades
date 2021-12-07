5ece5710fc11cb36b77b9bf48d875bd717fa7684
ofgonen <ofgonen@gmail.com>
2021-12-07 22:52:24 +0300

    Create EnemySet
    - Script & Asset

---
+++ Assets/Script/EnemySet.cs
@@ -0,0 +1,11 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "set_enemy", menuName = "FF/Data/Sets/Enemy_Set" ) ]
+public class EnemySet : RuntimeSet< int, Enemy >
+{
+}
