d9741edffa426211431634b307985870f1c55021
ofgonen <ofgonen@gmail.com>
2021-12-13 15:46:59 +0300

    Implemement SpawnPoint initial
    - RunTimeSet class
    - Pool asset

---
+++ Assets/Script/SpawnPoint.cs
@@ -0,0 +1,12 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using NaughtyAttributes;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [ BoxGroup( "Shared Variables" ) ] public SpawnPointSet spawn_point_set;
+}
